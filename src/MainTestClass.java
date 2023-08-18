cbdc5d6c5eb2420b5b40090b47bf9d82ea8d06c6
MohammadMassoud <silver.silver651@gmail.com>
2023-08-18 11:24:20 -0300

    alt

--- src/MainTestClass.java
+++ src/MainTestClass.java
@@ -3,25 +3,18 @@ import java.util.Scanner;
 public class MainTestClass{ 
 	public static void main(String args[]){ 
 		Scanner key = new Scanner(System.in);
-		double a;
-		double b;
-		char c;
-		double unused;
-		String t1;
-		a = key.nextDouble();
-		b = key.nextDouble();
-		a = 1;
-		b = 10;
-		t1 = "texto";
-		c = 't';
-				System.out.println(t1);
-
-				System.out.println(c);
-
-		if (a>b) {
-		System.out.println(a);
-		} else{
-		System.out.println(b);
+		double i;
+		i = 0;
+		while (i<3) {
+			System.out.println(i);
+			i = i+1;
 		}
+
+		i = 0;
+		do {
+			System.out.println(i);
+			i = i+1;
+		} while (i<3);
+
 	}
 }
