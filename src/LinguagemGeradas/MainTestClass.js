f7bbd254e8768f4e72d5c80e621398fa20ab2a70
MohammadMassoud <silver.silver651@gmail.com>
2023-08-19 10:40:04 -0300

    pasta ling gerdadas

---
+++ src/LinguagemGeradas/MainTestClass.js
@@ -0,0 +1,37 @@
+		let a;
+		let b;
+		let c;
+		let i;
+		let unused;
+		let t1;
+		a = prompt('Digite valor de a');
+		b = prompt('Digite valor de b');
+		i = 0;
+		a = 1;
+		b = 10;
+		t1 = "teste";
+		c = 't';
+		console.log(t1);
+
+		console.log(c);
+
+		if (a>b) {
+			console.log(a);
+			console.log(a);
+			console.log(a);
+		} else{
+			console.log(b);
+			console.log(a);
+			console.log(a);
+		}
+		while (i<3) {
+			console.log(i);
+			i = i+1;
+		}
+
+		i = 0;
+		do {
+			console.log(i);
+			i = i+1;
+		} while (i<3);
+
